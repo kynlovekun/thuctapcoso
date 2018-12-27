611e7938d080e4affe8cdc64c4f1cc771a44a652
kynlovekun <kynlovekun@gmail.com>
2018-12-27 23:58:59 +0700

    Update Bai3.cpp

--- Bai3.cpp
+++ Bai3.cpp
@@ -5,56 +5,56 @@
 #include <vector>
 using namespace std;
 
-struct De{	//khai bao struct
+struct De{										//khai bao cau truc De
 	string cau;
 	string A, B, C;
 	string DA;
 };
-vector<De> dsda;
+struct Player{ 									//khai bao cau truc nguoi choi
+	string ten;
+	int diem;
+};
+vector<De> dsda; 								//tao vector danh sach dap an
+vector<Player> dsplayer; 						// tao vector danh sach nguoi choi
 bool KiemTra(string a, string b){
 	if(a.compare(b) == 0)
 		return true;
 	return false;
 }
 void LD(string name, int score){
-	ofstream f("Luudiem.txt"); // luu diem
+	ofstream f("Luudiem.txt"); 					// luu diem
 	f<<name<<endl;
 	f<<"diem: "<<score;
 }
 void LayDe(){
-	ifstream f("DE1.txt"); // doc file text
+	ifstream f("DE1.txt"); 						// doc file text
 	int sl;
 	De de;
-	string s; // khai bao bien tam
-	getline(f,s); sl = atoi(s.c_str());			 // doi sl tu string sang int
+	string s; 									// khai bao bien tam
+	getline(f,s); sl = atoi(s.c_str());			// doi sl tu string sang int
 	for(int i = 0; i < sl; i++){ 				
-		getline(f,s); de.cau = s; cout<<s<<endl;
-		getline(f,s); de.A = s; cout<<s<<endl;		//gan gia tri vao bien tam s
-		getline(f,s); de.B = s; cout<<s<<endl;
-		getline(f,s); de.C = s; cout<<s<<endl;
+		getline(f,s); de.cau = s;
+		getline(f,s); de.A = s; 				//gan gia tri vao bien tam s
+		getline(f,s); de.B = s; 
+		getline(f,s); de.C = s; 
 		getline(f,s); de.DA = s;
 		dsda.push_back(de);
 	}
 }
-void Xuat(De bd){
-	ifstream f("DE1.txt");
-	int SL;
-	int diem = 0;
-	string dapan, x;
-	getline(f,x); SL = atoi(x.c_str()); 
-	for(int i = 0; i < SL; i++){
-		cout<<"Nhap cau tra loi "<<i+1<<": ";
-		cin>>dapan;
-		if(KiemTra(dapan, bd.DA) == true){
-			cout<<"That's right!"<<endl;
-			diem++;
-		}
-		else
-			cout<<"Oh Noooooooooo!"<<endl;
-	}
-	string ten;
-	LD(ten, diem);
-	
+void Xuat(De bd, int &diem){
+	string dapan;
+	cout<<bd.cau<<endl;
+	cout<<bd.A<<endl;
+	cout<<bd.B<<endl;
+	cout<<bd.C<<endl;
+	cout<<"Nhap cau tra loi: ";
+	cin>>dapan;
+}
+void DocDe(){
+	De bd;
+	Player n;
+	n.diem = 0;
+	for(int i = 0; i < bd.siz)
 }
 int main(){
 	De bode;
