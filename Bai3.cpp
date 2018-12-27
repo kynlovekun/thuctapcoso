f68d857d0d794b4e08701f75975fa1dcff9003f1
kynlovekun <kynlovekun@gmail.com>
2018-12-27 11:18:07 +0700

    Bai3

--- Bai3.cpp
+++ Bai3.cpp
@@ -16,23 +16,37 @@ bool KiemTra(string a, string b){
 		return true;
 	return false;
 }
-void DocDe(){
+void LD(string name, string score){
+	ofstream f("Luudiem.txt"); // luu diem
+	f<<name<<endl;
+	f<<"diem: "<<score;
+}
+void LayDe(){
 	ifstream f("DE1.txt"); // do file text
 	int sl;
 	De de;
-	string s, tl; // khai bao bien tam
-	getline(f,s); sl = atoi(s.c_str()); // doi sl tu string sang int
-	for(int i = 0; i < sl; i++){ //gan gia tri vao bien tam s
-		getline(f,s); de.cau = s; cout<<s<<endl;
-		getline(f,s); de.A = s; cout<<s<<endl;
-		getline(f,s); de.B = s; cout<<s<<endl;
-		getline(f,s); de.C = s; cout<<s<<endl;
-		getline(f,s); de.DA = s; cout<<s<<endl;
+	string s; // khai bao bien tam
+	getline(f,s); sl = atoi(s.c_str());			 // doi sl tu string sang int
+	for(int i = 0; i < sl; i++){ 				//gan gia tri vao bien tam s
+		getline(f,s); de.cau = s;
+		getline(f,s); de.A = s; 
+		getline(f,s); de.B = s; 
+		getline(f,s); de.C = s; 
+		getline(f,s); de.DA = s;
 		dsda.push_back(de);
-		cout<<"Nhap dap an: "; getline(cin, tl);
-		
-	} 
+	}
+}
+void Xuat(De bd){
+	int diem = 0;
+	string dapan;
+	cout<<bd.cau<<endl;
+	cout<<bd.A<<endl;
+	cout<<bd.B<<endl;
+	cout<<bd.C<<endl;
+	cout<<"Nhap cau tra loi: ";
+	cin>>dapan;
+	if()
 }
 int main(){
-	DocDe();
+	LayDe();
 }
