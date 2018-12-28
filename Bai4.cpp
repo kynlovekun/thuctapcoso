c5dcfc6012ed2982822316ddd5faa9108865a13f
kynlovekun <kynlovekun@gmail.com>
2018-12-28 09:32:30 +0700

    Bai4

--- Bai4.cpp
+++ Bai4.cpp
@@ -16,11 +16,28 @@ void XuatDT(int a[10][10],int n){					//xuat do thi trong file dothi.txt
 			cout<<endl;
 	}
 }
+void TaoMoi(int a[10][10], int &n1){					//tao moi mot do thi
+	cout<<"Nhap n1: ";
+	cin>>n1;
+	f.open("dothi1.txt", ios::out);					//mo file dothi1.txt de ghi
+	f<<n1<<endl;
+	for(int i = 0; i < n1; i++){
+		for(int j = 0; j < n1; j++){
+			cout<<"Trong so"<<i<<"den"<<j<<": ";
+			cin>>a[i][j];
+			f<<a[i][j]<<" ";								//ghi a[i][j] vao file dothi.txt
+		}
+		f<<endl;
+		f.close();
+	}
+}
 int main(){
-	int n;
+	int n, n1;
 	int a[10][10];
 	f.open("dothi.txt", ios::in);				// mo file dothi.txt de doc
 	f>>n;
 	DocFile(a,n);
 	XuatDT(a,n);
+	TaoMoi(a,n1);
+	XuatDT(a,n1);
 }
